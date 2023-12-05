5a0fca982189cbdec843a130c1b9f1b3c7fc168f
BumbleTree <77128029+BumbleTree@users.noreply.github.com>
2023-12-05 18:56:21 +0000

    Attempt to prevent suffocation
    Pls work

--- src/main/java/net/ioixd/PlayerCrouch.java
+++ src/main/java/net/ioixd/PlayerCrouch.java
@@ -3,19 +3,27 @@ package net.ioixd;
 import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
 
 public class PlayerCrouch implements ServerPlayNetworking.PlayChannelHandler {
     @Override
     public void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler,
-            PacketByteBuf buf, PacketSender responseSender) {
+                        PacketByteBuf buf, PacketSender responseSender) {
         Entity vehicle = player.getVehicle();
-        if (vehicle != null) {
-            if (vehicle instanceof PokemonEntity living) {
+        if (vehicle != null && vehicle instanceof PokemonEntity) {
+            // Check for two air blocks above the player
+            BlockPos playerPos = player.getBlockPos();
+            boolean isTwoAirBlocksAbove = player.getWorld().getBlockState(playerPos.up()).isOf(Blocks.AIR)
+                                          && player.getWorld().getBlockState(playerPos.up(2)).isOf(Blocks.AIR);
+
+            if (isTwoAirBlocksAbove) {
+                PokemonEntity living = (PokemonEntity) vehicle;
                 living.initGoals();
             }
         }
