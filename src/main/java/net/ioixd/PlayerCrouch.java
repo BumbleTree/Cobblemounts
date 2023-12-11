55ba5eb6a40336fc8b9c2a4ebdea528d8b1811fd
BumbleTree <mylesduncanking@gmail.com>
2023-12-11 20:12:02 +0000

    Remove failed crouch

--- src/main/java/net/ioixd/PlayerCrouch.java
+++ src/main/java/net/ioixd/PlayerCrouch.java
@@ -8,26 +8,15 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.math.BlockPos;
 
 public class PlayerCrouch implements ServerPlayNetworking.PlayChannelHandler {
     @Override
     public void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler,
                         PacketByteBuf buf, PacketSender responseSender) {
         Entity vehicle = player.getVehicle();
-        if (vehicle != null && vehicle instanceof PokemonEntity) {
-            // Check for two air blocks above the player
-            BlockPos playerPos = player.getBlockPos();
-            boolean isTwoAirBlocksAbove = player.getWorld().isAir(playerPos.up()) && player.getWorld().isAir(playerPos.up(2));
-
-            if (isTwoAirBlocksAbove) {
-                // Existing logic here
-                PokemonEntity living = (PokemonEntity) vehicle;
+        if (vehicle != null) {
+            if (vehicle instanceof PokemonEntity living) {
                 living.initGoals();
-            } else {
-                // Send a message to the player
-                player.sendMessage(Text.literal("BumbleTree won't let you dismount here or you may suffocate!"), false);
             }
         }
     }
